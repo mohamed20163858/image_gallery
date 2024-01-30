d8a1118aff726861baf2e76176b6e2ab105ef1ee
Mohamed Saleh <mohamed20163858@gmail.com>
2024-01-31 01:22:56 +0200

    Add isLoading property

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import ImageSearch from "./components/ImageSearch";
 function App() {
   const [images, setImages] = useState([]);
   const [term, setTerm] = useState("");
-
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
+    setIsLoading(true);
     async function fetchImages() {
       const response = await fetch(
         `https://pixabay.com/api/?key=${process.env.REACT_APP_API_KEY}&q=${term}&image_type=photo&pretty=true`
       );
       const data = await response.json();
       setImages(await data.hits);
+      setIsLoading(false);
     }
     fetchImages();
   }, [term]);
@@ -22,7 +24,8 @@ function App() {
       <ImageSearch setTerm={setTerm} />
       {/* ImageCards */}
       <div className=" flex justify-center gap-5 flex-wrap xl:grid xl:grid-cols-3 xl:place-items-center xl:gap-7 2xl:w-[80%] ">
-        {images &&
+        {!isLoading &&
+          images &&
           images.map((image) => (
             <ImageCard
               key={image.id}
@@ -35,7 +38,10 @@ function App() {
             />
           ))}
       </div>
-      {images.length === 0 && <h1>No Results Found</h1>}
+      {!isLoading && images.length === 0 && (
+        <h1 className="text-lg font-semibold">No Results Found</h1>
+      )}
+      {isLoading && <h1 className="text-xl font-bold">Loading Images....</h1>}
     </div>
   );
 }
